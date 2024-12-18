af51b28b27480e146190b0983b71098ca4e6fff3
QuietWind01 <calebhesse42@gmail.com>
2024-12-17 23:53:27 -0600

    Fixed favicon

--- 0/new/allowlist_user.php
+++ 0/new/allowlist_user.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="/stylesheets/compiled/main.css">
     <script type="text/javascript" src="/src/scripts/jquery/jquery-3.7.1.js"></script>
     <script type="text/javascript" src="/src/scripts/navbar.js" defer></script>
-    <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/assets/favicon_transparent.ico" type="image/x-icon">
 </head>
 <body id="main">
     <?php require($_SERVER["DOCUMENT_ROOT"]."/inc/navbar.php"); ?>
